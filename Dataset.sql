e4761e0362004f3058f0caf77280c5c0b06e249d
Sagar pagar <pagars857@gmail.com>
2025-09-11 08:56:57 +0530

    Dataset Analysis.sql

--- Dataset.sql
+++ Dataset.sql
@@ -21,3 +21,28 @@ select count(distinct Model) from car_price_dataset where Brand = "Toyota";
 select count(*) from car_price_dataset where Brand = 'Audi'  and Fuel_Type = 'Electric';
 
 
+use cars;
+ select * from car_price_dataset;
+ -- in car_price_dataset it finds the average price by specfic fuel type--
+ select Fuel_Type,count(Fuel_Type) as count, avg(Price)
+ from car_price_dataset
+ group by Fuel_Type;
+
+-- used to find the engine size by its specific model
+select Model,count(Engine_Size)
+from car_price_dataset
+group by Model
+having count(Engine_Size)> 315
+order by count(Engine_Size);
+
+--it counts the how many model make by compay in a specific year
+select Year,count(distinct Model)
+from car_price_dataset 
+group by Year;
+
+--it finds which brands model has minimun price
+select Brand, Model, min(Price)
+from car_price_dataset
+group by Brand,Model;
+
+
